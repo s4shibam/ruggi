13370cbed24e89cea3b96abeb4b17156fe80f6f7
s4shibam <s4shibam@gmail.com>
2025-07-07 09:23:45 +0530

    feat: add project setup and configuration files

---
+++ web/vite.config.ts
@@ -0,0 +1,26 @@
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+import path from 'node:path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true
+    }),
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: '.dist'
+  }
+})
